14df0a65606ded93368091ac3febf34ac7fc5e6a
Jeremy Dinkins <jeremydinkins@jeremys-macbook-pro.local>
2018-05-09 17:11:00 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jeremy Dinkins on 5/9/18.
+//  Copyright © 2018 Jeremy Dinkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
